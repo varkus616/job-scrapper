700362e01b3ae84ae4d3cf7bc7380eeb2e8de619
Gabriel Kania <DrRollingStone956>
2025-02-13 20:42:26 +0100

    dupa

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
-    pass
+    # Python program Continued
+# Webdriver For Firefox
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://mbasic.facebook.com")
+html = driver.page_source # Getting Source of Current URL / Web-Page Loaded
+print(html)
+# End
